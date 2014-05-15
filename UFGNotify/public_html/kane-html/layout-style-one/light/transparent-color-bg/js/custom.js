b17cf7080cff70f96671ab0a9bbad84ffb98187e
Laerte Marçal <laerte.marcal@hotmail.com>
2014-05-14 22:39:33 -0300

    Desabilitado ativação do menu ao rodar o scroll. (Menu agora está fixado)

--- UFGNotify/public_html/kane-html/layout-style-one/light/transparent-color-bg/js/custom.js
+++ UFGNotify/public_html/kane-html/layout-style-one/light/transparent-color-bg/js/custom.js
@@ -51,31 +51,31 @@ $('.main-navigation a').on('click', function(){
 
 /* NAVIGATION VISIBLE ON SCROLL */
 
-$(document).ready(function () {
-    mainNav();
-});
-
-$(window).scroll(function () {
-    mainNav();
-});
-
-if (matchMedia('(min-width: 992px), (max-width: 767px)').matches) {
-  function mainNav() {
-        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        if (top > 40) $('.sticky-navigation').stop().animate({"top": '0'});
-
-        else $('.sticky-navigation').stop().animate({"top": '-60'});
-    }
-}
-
-if (matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
-  function mainNav() {
-        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        if (top > 40) $('.sticky-navigation').stop().animate({"top": '0'});
-
-        else $('.sticky-navigation').stop().animate({"top": '-120'});
-    }
-}
+//$(document).ready(function () {
+//    mainNav();
+//});
+//
+//$(window).scroll(function () {
+//    mainNav();
+//});
+//
+//if (matchMedia('(min-width: 992px), (max-width: 767px)').matches) {
+//  function mainNav() {
+//        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+//        if (top > 40) $('.sticky-navigation').stop().animate({"top": '0'});
+//
+//        else $('.sticky-navigation').stop().animate({"top": '-60'});
+//    }
+//}
+//
+//if (matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
+//  function mainNav() {
+//        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+//        if (top > 40) $('.sticky-navigation').stop().animate({"top": '0'});
+//
+//        else $('.sticky-navigation').stop().animate({"top": '-120'});
+//    }
+//}
 
 
 
